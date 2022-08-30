2c5a565db28cf22ff9a872f66ded0488fadb1f74
DoanMinhPhuong <doanminhphuong.208@tcu.edu.vn>
2022-08-30 17:20:49 +0700

    page index

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,44 @@ window.addEventListener('DOMContentLoaded', function () {
     slidesPerColumn: 2,
     //slidesPerColumnFill: "row",
     slidesPerGroup: 6,
-    spaceBetween: 30,
+    spaceBetween: 25,
+    resistanceRatio: 0,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 6,
+        slidesPerColumn: 2,
+        spaceBetween: 25,
+      },
+    },
     pagination: {
       el: '.section-tourist .swiper-container .swiper-pagination',
       clickable: true,
@@ -18,6 +55,33 @@ window.addEventListener('DOMContentLoaded', function () {
       el: '.section-feature-guideline  .swiper-container .swiper-pagination',
       clickable: true,
     },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 5,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+    },
   });
 
   var swiper = new Swiper('.section-feature-guideline-3 .swiper-container', {
@@ -28,6 +92,32 @@ window.addEventListener('DOMContentLoaded', function () {
       el: '.section-feature-guideline-3  .swiper-container .swiper-pagination',
       clickable: true,
     },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
   });
 
   var swiper = new Swiper('.section-feature-guideline-4 .swiper-container', {
@@ -37,6 +127,42 @@ window.addEventListener('DOMContentLoaded', function () {
     pagination: {
       el: '.section-feature-guideline-4  .swiper-container .swiper-pagination',
       clickable: true,
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 25,
+      },
     },
   });
 
@@ -51,6 +177,38 @@ window.addEventListener('DOMContentLoaded', function () {
     navigation: {
       nextEl: '.swiper-feature .swiper-button-next',
       prevEl: '.swiper-feature .swiper-button-prev',
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 1,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 25,
+      },
     },
   });
 });
